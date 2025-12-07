7c94415e4fdd5e33e1ccf39081856e256758b75c
GiaPham <giapham@192.168.2.6>
2025-12-07 13:30:33 +0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,78 @@
+DOMAIN_SELECTOR_MAP = {
+    "vov.vn": {
+        "title": ".article-title.mb-0",                  # selector tiêu đề
+        "body": ".detail.detail--normal"             # selector nội dung
+    },
+    "baogialai.com.vn": {
+        "title": ".article__title.cms-title",                # selector tiêu đề
+        "body": ".article__body.zce-content-body.cms-body"
+    },
+    "congly.vn": {
+        "title": ".sc-longform-header-title.block-sc-title",                               # selector tiêu đề chung
+        "body": ".b-maincontent"
+    },
+    "baotintuc.vn": {
+        "title": ".sc-longform-header-title.block-sc-title",                               # selector tiêu đề chung
+        "body": ".boxdetail"
+    },
+    "qdnd.vn": {
+        "title": ".post-title",                               # selector tiêu đề chung
+        "body": ".article-detail"
+    },
+    "plo.vn": {
+        "title": ".article__title.cms-title",                               # selector tiêu đề chung
+        "body": ".l-content"
+    },
+    "baohatinh.vn" : {
+        "title": ".article__title.cms-title ",                               # selector tiêu đề chung
+        "body": ".article__body.zce-content-body.cms-body"
+    },
+    "dttc.sggp.org.vn": {
+        "title": ".article__title.cms-title",  # selector tiêu đề chung
+        "body": ".article__body.zce-content-body.cms-body"
+    },
+    "nld.com.vn": {
+        "title": ".detail-title",  # selector tiêu đề chung
+        "body": ".detail-content.afcbc-body  "
+    },
+    "www.vietnamplus.vn": {
+        "title": ".article__title.cms-title ",  # selector tiêu đề chung
+        "body": ".main-col.content-col"
+    },
+    "baoninhbinh.org.vn": {
+        "title": ".header-title.text-4xl.font-bold.font-serif.max-md\\:text-2xl.mb-2",
+        "body": "#content_detail"
+    },
+    "tapchicongthuong.vn" : {
+        "title": ".post-title.text-left.font-playfair",
+        "body": ".detail"
+    },
+    "suckhoedoisong.vn" : {
+        "title": ".detail-title",
+        "body": ".detail-content.afcbc-body"
+    },
+    "vnbusiness.vn": {
+        "title": ".title_news",
+        "body": ".fck_detail"
+    },
+    "bnews.vn" : {
+        "title": ".post-top-entry h1",
+        "body": ".post-ct-entry"
+    },
+    "tienphong.vn" : {
+        "title": ".article__title.cms-title",
+        "body": ".article__body.zce-content-body.cms-body"
+    },
+    "vtcnews.vn" : {
+        "title": ".font28.bold.lh-1-3",
+        "body": ".edittor-content.box-cont.mt15.clearfix"
+    },
+    "cand.com.vn" : {
+        "title": ".box-title-detail.entry-title",
+        "body": ".detail-content-body"
+    },
+    "baoquocte.vn" : {
+        "title": ".article-detail-title.f0",
+        "body": ".article-detail.fw.lt.mb.clearfix"
+    }
+}
